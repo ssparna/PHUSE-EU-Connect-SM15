d7da715dbab9562d31864460b8b4c8c2d26fa660
ssparna <134319704+ssparna@users.noreply.github.com>
2024-11-09 01:06:58 +0100

    attempt to satisfy sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ sys.path.insert(0, os.path.abspath("./annotation_exporter/main.py"))
 
 print(os.path.abspath("./annotation_exporter/annot_export.py"))
 
-autodoc_mock_imports = ["annotation_exporter"]
+autodoc_mock_imports = ["annotation_exporter", "annot_export", "generic", "main"]
 
 #import annot_exp
 
